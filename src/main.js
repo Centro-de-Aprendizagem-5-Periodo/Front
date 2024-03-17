8d7b06cc21d38d7d966fafb4a157957aa43f31d5
TechSneeker <fellipecarreiro2@gmail.com>
2024-03-17 19:12:43 -0300

    Adicionando configuração para possibilitar inclusão de icones

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import './assets/global.scss'
+import { createApp } from 'vue';
+import './assets/global.scss';
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).use(router).mount("#app");
+library.add(faPhone)
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(router).mount("#app");
