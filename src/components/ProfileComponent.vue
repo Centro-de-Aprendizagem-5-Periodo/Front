01af96012687033f2c8a52a2f1c3f0e56c5ff50a
TechSneeker <fellipecarreiro2@gmail.com>
2024-03-21 20:44:11 -0300

    Adicionando vida ao componente de perfil (edição em funcionamento)

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -1,12 +1,32 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const editingName = ref(false);
+const name = ref('Nome');
+const newName = ref('');
+
+function toggleEdit() {
+    editingName.value = !editingName.value;
+    if (!editingName.value) {
+        name.value = newName.value;
+    }
+}
+
+function handleEnter(event) {
+    if (event.key === 'Enter') {
+        toggleEdit();
+    }
+}
+</script>
 
 <template>
     <div id="profile">
         <img src="https://st3.depositphotos.com/1594920/18263/i/450/depositphotos_182639510-stock-photo-sphynx-hairless-cat-4-years.jpg">
-        <p>
-            Name
-            <font-awesome-icon id="icon" :icon="['fas', 'pencil']" style="color: #ffffff;" />
+        <p v-if="!editingName">
+            {{ name }}
+            <font-awesome-icon id="icon" :icon="['fas', 'pencil']" style="color: #ffffff;" @click="toggleEdit"/>
         </p>
+        <input v-else v-model="newName" @keyup.enter="handleEnter">
     </div>
 </template>
 
@@ -15,7 +35,7 @@
 
 #profile {
     display: grid;
-    justify-content: center;
+    justify-items: center;
     text-align: center;
     margin-top: 50px;
     gap: 10px;
@@ -34,6 +54,18 @@ p {
 
 #icon {
     padding-left: 4px;
+    cursor: pointer;
+}
+
+input {
+    font-family: $font-style;
+    color: white;
+    background-color: transparent;
+    border: none;
+    border-bottom: 1px solid white;
+    width: 125px;
+    text-align: center;
+    outline: none;
 }
 
 </style>
