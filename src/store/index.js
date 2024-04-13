b0fa6452885956ed4548333afde4ec173e05f95e
PedroHenriqueFonsec <pedrohenriquepereiradafonseca@gmail.com>
2024-04-13 19:19:43 -0300

    Escondido a ScrollBar e ajustado a edição do nome do perfil

--- src/store/index.js
+++ src/store/index.js
@@ -4,21 +4,31 @@ const store = createStore({
   state() {
     return {
       navbarOpen: false,
+      nameChanged: ('Nome'),
     };
   },
   mutations: {
     toggleNavbar(state) {
       state.navbarOpen = !state.navbarOpen;
     },
+    modifyName(state, newName) {
+      state.nameChanged = newName;
+    },
   },
   actions: {
     toggleNavbar({ commit }) {
       commit('toggleNavbar');
     },
+    modifyName({ commit }, newName) {
+      commit('modifyName', newName);
+    },
   },
   getters: {
     isNavbarOpen(state) {
       return state.navbarOpen;
+    },
+    isNameChanged(state) {
+      return state.nameChanged;
     },
   },
 });
