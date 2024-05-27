49fde2d1fe6f74c5a1d28a894b16930609b9b186
Alethos-BK <odahcamperiles@gmail.com>
2024-05-26 21:35:57 -0300

    Voltando como estava para testar separado o deploy

--- cypress/e2e/meus_certificados.cy.js
+++ cypress/e2e/meus_certificados.cy.js
@@ -1,15 +1,15 @@
 import { CursosPageObject } from "./page_objects/curso_page_object"
 import { click } from './utils/commands'
 
-// describe('pagina meus certificados', () => {
-//   const page = new CursosPageObject();
+describe('pagina meus certificados', () => {
+    const page = new CursosPageObject();
 
-//   beforeEach(() => {
-//     page.visit()
-//     click(page.myCertificatesText)
-//   })
+    beforeEach(() => {
+        page.visit()
+        click(page.myCertificatesText)
+    })
 
-//   // it('Abrir card de curso ao clicar no título do curso', () => {
-    
-//   // })
-// })
+    // it('Abrir card de curso ao clicar no título do curso', () => {
+
+    // })
+})
--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -1,58 +1,58 @@
 import { CursosPageObject } from './page_objects/curso_page_object'
 import { click, assertVisible, assertNotVisible, assertChildWithAttribute, getChild, assertText, clickButtonWithText } from './utils/commands'
 
-// describe('navbar', () => {
-//   const page = new CursosPageObject()
+describe('navbar', () => {
+    const page = new CursosPageObject()
 
-//   beforeEach(page.visit)
+    beforeEach(page.visit)
 
-//   it('botao de hamburger deve abrir navbar', () => {
-//     const { navbar } = page
-//     click(navbar.closeButton)
-//     click(navbar.openButton)
-//     assertVisible(navbar.div)
-//   })
+    it('botao de hamburger deve abrir navbar', () => {
+        const { navbar } = page
+        click(navbar.closeButton)
+        click(navbar.openButton)
+        assertVisible(navbar.div)
+    })
 
-//   it('botao sair deve deslogar usuario', () => {
-//     click(page.logoutButton)
-//     // aguardando implementacao
-//   })
+    it('botao sair deve deslogar usuario', () => {
+        click(page.logoutButton)
+        // aguardando implementacao
+    })
 
-//   it('botao x deve fechar navbar', () => {
-//     const { navbar } = page
-//     click(navbar.closeButton)
-//     assertNotVisible(navbar.div)
-//   })
+    it('botao x deve fechar navbar', () => {
+        const { navbar } = page
+        click(navbar.closeButton)
+        assertNotVisible(navbar.div)
+    })
 
-//   it('deve conter imagem', () => {
-//     assertChildWithAttribute({
-//       parentSelector: page.profileDiv,
-//       childSelector: 'img',
-//       attributeName: 'src',
-//     })
-//   })
+    it('deve conter imagem', () => {
+        assertChildWithAttribute({
+            parentSelector: page.profileDiv,
+            childSelector: 'img',
+            attributeName: 'src',
+        })
+    })
 
-//   it('deve conter nome', () => {
-//     getChild(page.profileDiv, 'p')
-//   })
+    it('deve conter nome', () => {
+        getChild(page.profileDiv, 'p')
+    })
 
-//   it('botao com icone de caneta deve permitir mudar informacoes do usuario', () => {
-//     click(page.editButton)
-//     // aguardando implementacao
-//   })
+    it('botao com icone de caneta deve permitir mudar informacoes do usuario', () => {
+        click(page.editButton)
+        // aguardando implementacao
+    })
 
-//   it('botao meus cursos abre pagina de meus cursos', () => {
-//     clickButtonWithText(page.myCoursesText)
-//     assertText(page.sectionTitle, page.myCoursesText)
-//   })
+    it('botao meus cursos abre pagina de meus cursos', () => {
+        clickButtonWithText(page.myCoursesText)
+        assertText(page.sectionTitle, page.myCoursesText)
+    })
 
-//   it('botao meus certificados abre pagina de meus certificados', () => {
-//     clickButtonWithText(page.myCertificatesText)
-//     assertText(page.sectionTitle, page.myCertificatesText)
-//   })
+    it('botao meus certificados abre pagina de meus certificados', () => {
+        clickButtonWithText(page.myCertificatesText)
+        assertText(page.sectionTitle, page.myCertificatesText)
+    })
 
-//   it('botao todos os cursos abre pagina de todos os cursos', () => {
-//     clickButtonWithText(page.allCoursesText)
-//     assertText(page.sectionTitle, page.allCoursesText)
-//   })
-// })
+    it('botao todos os cursos abre pagina de todos os cursos', () => {
+        clickButtonWithText(page.allCoursesText)
+        assertText(page.sectionTitle, page.allCoursesText)
+    })
+})
