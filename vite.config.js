81e1b99efae09cf19261f34b57f495fb96b993c9
Alethos-BK <odahcamperiles@gmail.com>
2024-05-27 21:54:32 -0300

    Respeitem a tia alice depois desse commit -  adiconando gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://centro-de-aprendizagem-5-periodo.github.io/Front/',
   plugins: [vue()]
 })
